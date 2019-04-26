66f08a5b5a80d78acd7f1724a8e1db9217bf14e2
Mirna <MirnaMuhammad98@gmail.com>
2019-04-26 02:01:09 +0200

    kmalloc() added

--- kheap.c
+++ kheap.c
@@ -4,11 +4,77 @@
 
 //NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
 
+#define KERNEL_HEAP_SIZE (KERNEL_HEAP_MAX - KERNEL_HEAP_START)
+uint32 allocation_counter = 0; //For indexing: allocated_mem[]
+struct AllocatedMemory
+{
+	uint32 virtual_address;
+	uint32 allocated_pages;
+}allocated_mem[KERNEL_HEAP_SIZE/PAGE_SIZE + 1];
+
 void* kmalloc(unsigned int size)
 {
 	//TODO: [PROJECT 2019 - MS1 - [1] Kernel Heap] kmalloc()
-	return (void*);
+	uint32 required_num_pages = size/PAGE_SIZE + (size % PAGE_SIZE != 0);
+
+	uint32 allocation_va = -1, best_allocation = KERNEL_HEAP_SIZE/PAGE_SIZE + 1;
+
+	uint32 *ptr_table;
+	struct Frame_Info* ptr_frame_info;
+
+	uint32 consecutive_pages = 0, tmp_start = -1;
+	for(uint32 va = KERNEL_HEAP_START ; va < KERNEL_HEAP_MAX ; va += PAGE_SIZE) {
+		ptr_frame_info = get_frame_info(ptr_page_directory, (void *)va, &ptr_table);
+		//The VA is mapped
+		if(ptr_frame_info != NULL) {
+			if(consecutive_pages >= required_num_pages && best_allocation > consecutive_pages) {
+				allocation_va = tmp_start;
+				best_allocation = consecutive_pages;
+			}
+			tmp_start = -1;
+			consecutive_pages = 0;
+			continue;
+		}
+		if(!consecutive_pages) //Fresh start on a new free range
+			tmp_start = va;
+		consecutive_pages++;
+	}
+	//Last Check
+	if(consecutive_pages >= required_num_pages && best_allocation > consecutive_pages) {
+		allocation_va = tmp_start;
+		best_allocation = consecutive_pages;
+	}
+	if(allocation_va == -1) return (void*)NULL; //No suitable address was found
+
+	for(uint32 i=0, va = allocation_va ; i < required_num_pages ; i++, va += PAGE_SIZE) {
+		ptr_frame_info = get_frame_info(ptr_page_directory, (void *)va, &ptr_table);
+		int result;
+		if(ptr_frame_info == NULL)
+			result = allocate_frame(&ptr_frame_info);
+
+		if(result == E_NO_MEM){
+			cprintf("FAILED, no physical memory available.\n");
+			return (void*)NULL;
+		}
+
+		result = map_frame(ptr_page_directory, ptr_frame_info, (void*)va, PERM_WRITEABLE|PERM_PRESENT);
+		if(result != 0){
+			cprintf("FAILED, no page table found and there’s no free frame for creating it.\n");
+			free_frame(ptr_frame_info);
+			return (void*)NULL;
+		}
+	}
+	allocated_mem[allocation_counter].allocated_pages = required_num_pages;
+	allocated_mem[allocation_counter].virtual_address = allocation_va;
+	allocation_counter++;
+
+	//TODO: [PROJECT 2019 - BONUS1] Implement the FIRST FIT strategy for Kernel allocation
+	// Beside the BEST FIT
+	// use "isKHeapPlacementStrategyFIRSTFIT() ..." functions to check the current strategy
+
+	return (void*)allocation_va;
 }
+
 
 void kfree(void* virtual_address)
 {
